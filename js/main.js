c7e8e574c9b92bae6e9081b4de98b2811c755c5c
GabrielT0maz <jgabrieltomaz2706@gmail.com>
2019-12-17 11:57:58 -0300

    Agora funciona todas as operações.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function insert(num){
+    var numero = document.getElementById('captura-num').value;
+    document.getElementById('captura-num').value =  numero + num;
+}
+function igual(){
+    var valor = document.getElementById('captura-num').value;
+    if(valor){
+        document.getElementById('resultado').value = eval(valor);
+    }
+}
+function limpar(){
+    document.getElementById('captura-num').value = "";
+}
+function apagar(){
+    var valor = document.getElementById('captura-num').value;
+    document.getElementById('captura-num').value = valor.substring(0,valor.length-1);
+}
