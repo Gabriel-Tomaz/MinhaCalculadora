21a432b3189a20cc309b80a2fc4370b0d76dfd88
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2020-12-19 16:05:12 -0300

    arrumando bug do botão de limpar

--- js/main.js
+++ js/main.js
@@ -2,16 +2,19 @@ function getNumber(num){
     const number = document.querySelector('#number').value;
     document.querySelector('#number').value = number + num;
 }
+
 function equal(){
     const expression = document.querySelector('#number').value;
     if(expression){
         document.querySelector('#result').value = eval(expression);
     }
 }
-function clear(){
+
+function clean(){
     document.querySelector('#number').value = "";
     document.querySelector('#result').value = "";
 }
+
 function del(){
     const expression = document.querySelector('#number').value;
     document.querySelector('#number').value = expression.substring(0,expression.length-1);
